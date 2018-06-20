447ebb08a3501b130f1684d29060ac6f14f8d71a
Rishabh Ravindra <rishabhravindra@gmail.com>
2018-06-20 03:30:49 -0400

    Test react wrapper for material design

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from 'rmwc/Button'
+
+const TestButton = () => (
+    <React.Fragment>
+        <Button>Hello!</Button>
+    </React.Fragment>
+);
+
+export default TestButton;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'material-components-web/dist/material-components-web.min.css'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
 registerServiceWorker();
